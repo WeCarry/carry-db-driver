00e3b7ee89e6f3306a6103b5210de49a105abd15
fayyozbobokulov <b.fayyoz2000@gmail.com>
2023-03-20 00:08:01 +0500

    Corrected package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export * from "./database";
+export { Database, DatabaseConfig } from "./database";
+export { MongoDBAdapter } from "./adapters/mongodb.adapter";
+export { BaseRepository, IRepository } from "./repositories/base.repository";
